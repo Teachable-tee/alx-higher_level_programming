dee5b965534567c3e29bd673621d92dfd69b1c9e
Oluwatosin <jinaduoluwatosin@gmail.com>
2022-11-30 13:46:30 -0800

    hello

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,4 @@
-#include “lists.h”
+#include "lists.h"
 
 /**
  * check_cycle - chek if the list contains a cycle
@@ -8,17 +8,17 @@
  */
 int check_cycle(listint_t *list)
 {
-        listint_t *slow = list;
-        listint_t *fast = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-        if (!list)
-                return (0);
-        while (slow && fast && fast->next)
-        {
-                slow = slow->next;
-                fast = fast->next->next;
-                if (slow == fast)
-                        return (1);
-        }
-        return (0);
+	if (!list)
+		return (0);
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
 }
