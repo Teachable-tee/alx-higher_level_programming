6d1151223ae7c6df79416649950061f8fecc6860
Oluwatosin <jinaduoluwatosin@gmail.com>
2022-12-22 12:15:05 -0800

    structure

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(martix = []):
+    new_matrix = matrix.copy()
+
+    for i in
+    range(len(matrix)):
+        new matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+        return (new_matrix):
